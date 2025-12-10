53c248fa8afdaee55a0d5ce93e35e7539a502747
Antonio Navarro <dojeDulDev@Proton.me>
2025-12-10 10:48:49 +0100

    feat: add scroll-to-up button

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+import { scrollUp, toggleScrollButton } from "./scrollMobilePage.js";
+
+const scrollUpButton = document.getElementById("btn-scroll-to-top");
+
+// Añadimos el evento onclick al boton para hacer scroll al inicio de la página.
+scrollUpButton.addEventListener("click", () => {
+  scrollUp();
+});
+
+window.addEventListener("scroll", () => {
+  toggleScrollButton();
+});
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   loadMobileBUtton();
+//   loadPaginationButtons();
+//   showPage(1);
+// });
